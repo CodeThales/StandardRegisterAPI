4ae3947cedc3d81030cfefbd3277027783b85325
CodeThales <thalles.agency@gmail.com>
2023-07-07 08:46:11 -0300

    update security basic auth to send user and password in the Header request

--- registerAPI/src/main/java/com/thalescoding/registerAPI/config/SecurityConfig.java
+++ registerAPI/src/main/java/com/thalescoding/registerAPI/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.thalescoding.registerAPI.config;
 
+import com.thalescoding.registerAPI.service.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,41 +8,59 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    //Validação de segurança em memória
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-            .authorizeHttpRequests(requests ->
-                requests.requestMatchers("/**")
-                        .hasAnyRole("USER", "MANAGER", "ADMIN", "SUPER_ADMIN")
-                        .anyRequest()
-                        .authenticated()
-            )
-            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-            .logout(LogoutConfigurer::permitAll);
+        final HttpSecurity config = http
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/**")
+                                .hasAnyRole("USER", "MANAGER", "ADMIN", "SUPER_ADMIN")
+                                .anyRequest()
+                                .authenticated()
+                )
+                //.formLogin(AbstractAuthenticationFilterConfigurer::permitAll) //Authorization pelo formulário
+                .httpBasic()//Aqui estamos usando basic auth (login e senha) ao invés do formLogin()
+                .and()
+                //.addFilter(new JwtAuthenticationFilter(authenticationManagerBean(),"seu_jwt_secret_aqui", 86400000))
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
 
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(new BCryptPasswordEncoder().encode("123"))
-                .roles("ADMIN");
+
+        //Autenticação de segurança em Banco
+        auth.userDetailsService(userDetailService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+
+
+        //Autenticação de segurança em memória
+//        auth.inMemoryAuthentication()
+//                .withUser("john@gmail.com")
+//                .password(new BCryptPasswordEncoder().encode("$2a$12$ayIKpNFysmse/wbLZUXFMupPXcb8SkH373bENmbSR/f8CgvOE.A0C"))
+//                .roles("ADMIN");
+
+        //john@gmail.com
+        //123
     }
+
 
     @Bean
     public AuthenticationEntryPoint authenticationEntryPoint() {
